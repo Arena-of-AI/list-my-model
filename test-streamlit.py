a6916d50dfa48d5f4a00a20f4c424b932597c0d2
Arena-of-AI <132893291+Arena-of-AI@users.noreply.github.com>
2023-05-19 18:18:59 +0800

    Update test-streamlit.py

--- test-streamlit.py
+++ test-streamlit.py
@@ -1,9 +1,7 @@
 import streamlit as st
 
-# 使用 st.beta_expander (Streamlit v0.87.0 或更高版本)
-with st.beta_expander("點擊展開內容"):
+with st.expander("點擊展開內容"):
     st.write("這是一大段內容...")
 
-# 或者使用 st.expander (Streamlit v0.86.0 或更低版本)
 with st.expander("點擊展開內容"):
     st.write("這是一大段內容...")
