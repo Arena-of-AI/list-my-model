b2c0f69aee29ddf858b5920de2adc60d0fb4b6ec
Arena-of-AI <132893291+Arena-of-AI@users.noreply.github.com>
2023-05-19 17:52:17 +0800

    Update test-streamlit.py

--- test-streamlit.py
+++ test-streamlit.py
@@ -4,15 +4,11 @@ import streamlit as st
 tabs = ["Tab 1", "Tab 2", "Tab 3"]
 
 # 在sidebar中创建选项卡
-selected_tab = st.sidebar.selectbox("Select Tab", tabs)
+selected_tab = st.sidebar.radio("Select Tab", tabs)
 
 # 根据选项卡显示相应的内容
-if selected_tab == "Tab 1":
-    st.header("Tab 1 Content")
-    st.write("This is the content of Tab 1.")
-elif selected_tab == "Tab 2":
-    st.header("Tab 2 Content")
-    st.write("This is the content of Tab 2.")
-elif selected_tab == "Tab 3":
-    st.header("Tab 3 Content")
-    st.write("This is the content of Tab 3.")
+for tab in tabs:
+    if tab == selected_tab:
+        st.sidebar.markdown(f"**{tab}**")
+    else:
+        st.sidebar.markdown(tab)
