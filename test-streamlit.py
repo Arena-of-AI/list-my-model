818a51381ab2b81a73e5dac01fd059b6fc1eeeba
Arena-of-AI <132893291+Arena-of-AI@users.noreply.github.com>
2023-05-23 23:24:28 +0800

    Update test-streamlit.py

--- test-streamlit.py
+++ test-streamlit.py
@@ -1,7 +1,37 @@
 import streamlit as st
 
-with st.expander("點擊展開內容"):
-    st.write("這是一大段內容...")
+def authenticate(password):
+    # 在這裡執行密碼驗證邏輯，例如比對密碼是否正確
+    return password == "your_password"
 
-with st.expander("點擊展開內容"):
-    st.write("這是一大段內容...")
+def main():
+    # 檢查 Session State 中是否已進行過驗證
+    if "authenticated" not in st.session_state:
+        st.session_state.authenticated = False
+
+    # 如果尚未進行驗證，請求使用者輸入密碼
+    if not st.session_state.authenticated:
+        password = st.text_input("請輸入密碼", type="password")
+        if st.button("驗證"):
+            if authenticate(password):
+                st.session_state.authenticated = True
+                st.success("驗證成功！")
+            else:
+                st.error("驗證失敗！請再試一次。")
+    else:
+        # 顯示多頁選擇的畫面
+        page = st.sidebar.selectbox("選擇頁面", ("頁面 1", "頁面 2", "頁面 3"))
+
+        # 根據選擇顯示相應的內容和按鈕
+        if page == "頁面 1":
+            st.title("頁面 1")
+            st.button("按鈕 1")
+        elif page == "頁面 2":
+            st.title("頁面 2")
+            st.button("按鈕 2")
+        elif page == "頁面 3":
+            st.title("頁面 3")
+            st.button("按鈕 3")
+
+if __name__ == "__main__":
+    main()
