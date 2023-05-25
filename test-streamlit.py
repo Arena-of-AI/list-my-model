268a5fc7ca3ba512cc62940abe0458079172fe98
Arena-of-AI <132893291+Arena-of-AI@users.noreply.github.com>
2023-05-25 11:54:49 +0800

    Update test-streamlit.py

--- test-streamlit.py
+++ test-streamlit.py
@@ -7,20 +7,19 @@ api_key = st.text_input("Enter your OpenAI API KEY")
 # 设置 OpenAI API 密钥
 openai.api_key = api_key
 
-# 获取现有模型列表
-response = openai.ChatCompletion.list_models()
+# 创建 ChatCompletion 实例
+chat_instance = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "What models do I have?"},
+    ]
+)
 
-# 解析响应数据
-if response and "data" in response:
-    models = response["data"]
-else:
-    st.error("Error fetching model list")
+# 获取现有模型列表
+models = chat_instance["choices"][0]["message"]["content"]
 
 # 显示模型列表
 st.title("Existing Models")
 for model in models:
-    st.write(f"- Model ID: {model['id']}")
-    st.write(f"  Model Name: {model['name']}")
-    st.write(f"  Model Description: {model['description']}")
-    st.write(f"  Model Owner: {model['owner']}")
-    st.write("---")
+    st.write(f"- Model: {model}")
