6eaa0ae1e5d403440ccbd914ede544ab033a53ad
Arena-of-AI <132893291+Arena-of-AI@users.noreply.github.com>
2023-05-25 12:01:52 +0800

    Update test-streamlit.py

--- test-streamlit.py
+++ test-streamlit.py
@@ -1,26 +1,21 @@
 import streamlit as st
 import openai
 
-# 输入 OpenAI API KEY
-api_key = st.text_input("Enter your OpenAI API KEY")
+# 设置OpenAI API密钥
+openai.api_key = "YOUR_API_KEY"
 
-# 设置 OpenAI API 密钥
-openai.api_key = api_key
+# 创建函数以获取并显示所有模型
+def list_models():
+    models = openai.Model.list()
+    for model in models:
+        st.write(f"Model Name: {model.name}")
+        st.write(f"Model ID: {model.id}")
+        st.write(f"Model Owner: {model.owner}")
+        st.write(f"Is Open Source?: {model.open_source}")
+        st.write("")
 
-# 获取现有模型列表
-response = openai.ChatCompletion.list_models()
+# 设置标题
+st.title("List of Models")
 
-# 解析响应数据
-if response and "data" in response:
-    models = response["data"]
-else:
-    st.error("Error fetching model list")
-
-# 显示模型列表
-st.title("Existing Models")
-for model in models:
-    st.write(f"- Model ID: {model['id']}")
-    st.write(f"  Model Name: {model['name']}")
-    st.write(f"  Model Description: {model['description']}")
-    st.write(f"  Model Owner: {model['owner']}")
-    st.write("---")
+# 调用函数以显示所有模型
+list_models()
