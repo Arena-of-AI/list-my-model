d3dc203fa6c4f8fc1a245f251d13f1b09341a35b
Arena-of-AI <132893291+Arena-of-AI@users.noreply.github.com>
2023-05-25 12:21:36 +0800

    Update test-streamlit.py

--- test-streamlit.py
+++ test-streamlit.py
@@ -1,22 +1,27 @@
 import streamlit as st
 import openai
 
+# 输入API密钥
 api_key = st.text_input("Enter your OpenAI API KEY")
 
-# 设置 OpenAI API 密钥
+# 设置OpenAI API密钥
 openai.api_key = api_key
 
-# 创建函数以获取并显示所有模型
+# 创建函数以获取并显示所有模型名称
 def list_models():
     # 获取模型列表
-    models = openai.Model.list()
+    model_lst = openai.Model.list()
 
-    # 显示模型信息
-    for model in models:
-        st.write(f"Model Name: {model}")
+    # 提取模型名称并显示
+    model_names = [model['id'] for model in model_lst['data']]
+    st.write("Available Models:")
+    for name in model_names:
+        st.write(name)
 
 # 设置标题
 st.title("List of Models")
 
-# 调用函数以显示所有模型
-list_models()
+# 检查是否输入了API密钥
+if api_key:
+    # 调用函数以显示所有模型名称
+    list_models()
