a4a7dd93f5d6d535eff4b680efcf2438b46519b5
Arena-of-AI <132893291+Arena-of-AI@users.noreply.github.com>
2023-05-26 12:16:30 +0800

    Update test-streamlit.py

--- test-streamlit.py
+++ test-streamlit.py
@@ -7,18 +7,18 @@ api_key = st.text_input("Enter your OpenAI API KEY")
 # 设置OpenAI API密钥
 openai.api_key = api_key
 
-# 创建函数以获取并显示所有Fine-Tuned模型
-def list_fine_tuned_models():
-    # 获取Fine-Tuned模型列表
-    models = openai.FineTune.list()
-    
+# 创建函数以获取并显示所有模型
+def list_models():
+    # 获取模型列表
+    models = openai.Model.list()
+
     # 输出终端机消息到Streamlit
     st.code(models)
 
 # 设置标题
-st.title("List of Fine-Tuned Models")
+st.title("List of Models")
 
 # 检查是否输入了API密钥
 if api_key:
-    # 调用函数以显示所有Fine-Tuned模型
-    list_fine_tuned_models()
+    # 调用函数以显示所有模型
+    list_models()
