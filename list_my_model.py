31c8011286ce09b3b5a73e5c70fcfaa4c570bc0c
Arena-of-AI <132893291+Arena-of-AI@users.noreply.github.com>
2023-05-27 11:46:09 +0800

    Update list_my_model.py

--- list_my_model.py
+++ list_my_model.py
@@ -8,60 +8,73 @@ api_key = st.text_input("Enter your OpenAI API KEY")
 # 设置OpenAI API密钥
 openai.api_key = api_key
 
-# 创建函数以获取并显示所有模型
+# 创建函数以获取并返回所有模型
 def list_models():
-    # 获取模型列表
-    models = openai.Model.list()
+    # 检查是否输入了API密钥
+    if not api_key:
+        st.warning("Please enter your OpenAI API KEY.")
+        return []
 
-    # 筛选并提取需要的字段
-    filtered_models = [
-        {
-            "Created at": datetime.fromtimestamp(model["created"]).strftime("%Y-%m-%d %H:%M:%S"),
-            "Model Name (ID)": model["id"],
-            "Parent Model": model["parent"]
-        }
-        for model in models['data']
-        if model['owned_by'] not in ['openai-internal', 'openai', 'system', 'openai-dev']
-    ]
+    try:
+        # 获取模型列表
+        models = openai.Model.list()
 
-    return filtered_models
+        # 筛选并提取需要的字段
+        filtered_models = [
+            {
+                "Created at": datetime.fromtimestamp(model["created"]).strftime("%Y-%m-%d %H:%M:%S"),
+                "Model Name (ID)": model["id"],
+                "Parent Model": model["parent"]
+            }
+            for model in models['data']
+            if model['owned_by'] not in ['openai-internal', 'openai', 'system', 'openai-dev']
+        ]
+
+        return filtered_models
+    except openai.error.AuthenticationError as e:
+        st.error(str(e))
+        return []
 
 # 设置标题
 st.title("List of Models")
 
-# 检查是否输入了API密钥
-if api_key:
-    # 调用函数以显示所有模型
-    models = list_models()
+# 调用函数以获取所有模型
+models = list_models()
 
-    # 检查模型列表是否为空
-    if models:
-        # 显示模型列表的表格
-        st.table(models)
+# 检查模型列表是否为空
+if models:
+    # 显示模型列表的表格
+    st.table(models)
 
-        # 添加对话框和删除按钮
-        model_name = st.text_input("To avoid accidental deletion, please type the model name you want to delete")
-        delete_button = st.button("Delete")
+    # 添加对话框和删除按钮
+    model_name = st.text_input("To avoid accidental deletion, please type the model name you want to delete")
+    delete_button = st.button("Delete")
 
-        # 当删除按钮被按下时执行删除操作
-        if delete_button:
-            # 确认删除的对话框
-            confirmation = st.confirm("Are you sure you want to delete the model?")
+    # 当删除按钮被按下时执行删除操作
+    if delete_button:
+        if model_name:
+            model_names = [model["Model Name (ID)"] for model in models]
+            if model_name in model_names:
+                # 显示确认对话框
+                confirmation = st.checkbox("Are you sure you want to delete the model?")
 
-            if confirmation:
-                # 检查要删除的模型名称是否存在
-                model_names = [model["Model Name (ID)"] for model in models]
-                if model_name in model_names:
-                    # 执行删除操作
+                # 当确认对话框为True时执行删除操作
+                if confirmation:
+                    # 删除模型
                     response = openai.Model.delete(model_name)
 
-                    # 等待删除操作完成并输出结果
-                    st.write(response)
+                    # 判断删除是否成功
                     if response.get("deleted"):
+                        # 等待终端响应，显示成功消息
+                        st.text("Deleting the model...")
                         st.success("Model deleted successfully.")
                     else:
+                        # 等待终端响应，显示失败消息
+                        st.text("Deleting the model...")
                         st.error("Failed to delete the model.")
-                else:
-                    st.warning("Please enter a valid model name.")
-    else:
-        st.info("This API key doesn't have any fine-tuned model.")
+            else:
+                st.warning("Please enter a valid model name.")
+        else:
+            st.warning("Please enter a model name to delete.")
+else:
+    st.info("This API key doesn't have any fine-tuned model.")
