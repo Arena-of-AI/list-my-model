ec6db57d0e878ae5c998d279ff2d60407ae6e114
Arena-of-AI <132893291+Arena-of-AI@users.noreply.github.com>
2023-05-27 11:16:16 +0800

    Update list_my_model.py

--- list_my_model.py
+++ list_my_model.py
@@ -8,7 +8,7 @@ api_key = st.text_input("Enter your OpenAI API KEY")
 # 设置OpenAI API密钥
 openai.api_key = api_key
 
-# 创建函数以获取并显示所有模型
+# 创建函数以获取并返回所有模型
 def list_models():
     # 获取模型列表
     models = openai.Model.list()
@@ -24,16 +24,18 @@ def list_models():
         if model['owned_by'] not in ['openai-internal', 'openai', 'system', 'openai-dev']
     ]
 
-    # 输出终端机消息到Streamlit
-    st.table(filtered_models)
+    return filtered_models
 
 # 设置标题
 st.title("List of Models")
 
 # 检查是否输入了API密钥
 if api_key:
-    # 调用函数以显示所有模型
-    list_models()
+    # 调用函数以获取所有模型
+    models = list_models()
+
+    # 显示模型列表的表格
+    st.table(models)
 
     # 添加对话框和删除按钮
     model_name = st.text_input("Enter the model name to delete")
@@ -42,7 +44,7 @@ if api_key:
     # 当删除按钮被按下时执行删除操作
     if delete_button:
         if model_name:
-            model_names = [model["id"] for model in filtered_models]
+            model_names = [model["id"] for model in models]
             if model_name in model_names:
                 openai.Model.delete(model_name)
                 st.success(f"Model {model_name} has been deleted.")
